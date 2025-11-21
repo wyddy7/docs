775d5fc93b175546d5397fa4a33ca3d8fb8c62cb
wyddy7 <dan47.r@gmail.com>
2025-11-21 14:54:17 +0300

    Remove Comments component from DocItem layout to streamline the rendering process.

--- website/src/theme/DocItem/Layout/index.tsx
+++ website/src/theme/DocItem/Layout/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Layout from '@theme-original/DocItem/Layout';
 import type LayoutType from '@theme/DocItem/Layout';
 import type {WrapperProps} from '@docusaurus/types';
-import Comments from '@site/src/components/Comments';
 
 type Props = WrapperProps<typeof LayoutType>;
 
@@ -10,7 +9,6 @@ export default function LayoutWrapper(props: Props): React.JSX.Element {
   return (
     <>
       <Layout {...props} />
-      <Comments />
     </>
   );
 }
